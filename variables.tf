5fd79c3a294248912799ab83cb809c2d54a6a16b
Cyril Ossai <85683554+seeewhy@users.noreply.github.com>
2024-05-11 17:26:15 +0100

    update variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 variable "my_ip" {
-   description = "My IP address"
+   description = "My Static IP address"
    type = string
    sensitive = true
 }
+
+variable "region" {
+   description = "Define the region where AWS service is provisioned"
+   default = "us-east-2"
+   }
