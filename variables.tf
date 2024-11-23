ca41730d20d265bade45a2fcba6e8c7f5b05dc21
Cyril Ossai <85683554+seeewhy@users.noreply.github.com>
2024-11-23 13:17:06 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,16 @@ variable "region" {
    description = "Define the region where AWS service is provisioned"
    default = "us-east-2"
    }
+
+
+variable "ip" {
+  description = "ip address for my workstation"
+  type        = string
+  default     = "158.272.8.178"
+}
+
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
